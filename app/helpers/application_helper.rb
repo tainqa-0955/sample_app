80814c5a81887aad18c35f09face47e8f6a5aff4
Nguyen Quang Anh Tai <nguyen.quang.anh.tai@sun-asterisk.com>
2019-09-17 14:20:42 +0700

    Chapter 5 Filling in the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def full_title page_title
+  def full_title page_title = ""
     base_title = I18n.t "static_pages.basic_title"
     if page_title.blank?
       base_title
