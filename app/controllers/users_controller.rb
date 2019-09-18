b64b92b01356aac854eeddcfaa8df7b105891dce
Nguyen Quang Anh Tai <nguyen.quang.anh.tai@sun-asterisk.com>
2019-09-18 11:37:13 +0700

    Chapter 7 Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by_id params[:id]
+    return if @user
+    flash[:danger] = t "static_pages.user.signup.not_found_user"
+    redirect_to signup_path
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "static_pages.user.signup.message_success"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
